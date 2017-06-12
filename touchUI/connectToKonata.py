08b6256593d6b4b784eced0afee20110a04d04ef
xztaityozx <taityonohibi@gmail.com>
2017-06-12 10:45:33 +0900

    fixed connect to servers

--- touchUI/connectToKonata.py
+++ touchUI/connectToKonata.py
@@ -1,21 +1,23 @@
 import paramiko
 import os
-
+from datetime import datetime
 
 def ConnectToKonata(code):
     HOST=os.environ["KONATA_HOST"]
     USER=os.environ["KONATA_USER"]
-    PRIVATE_KEY='/home/xztaityozx/.ssh/konata_rsa'
+    PRIVATE_KEY=os.environ["KONATA_KEY"]
     
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.load_system_host_keys()
     ssh.connect(HOST,username=USER,key_filename=PRIVATE_KEY)
     
-    command='echo -e "1\n'+code+'" > /home/ubuntu/zaisekiKun/zaishituKun/touchUI/dataBank.txt'
+    command='echo -e "1\n'+code+'" > $HOME/zaisekiKun/zaishituKun/touchUI/dataBank.txt'
     
     ssh.exec_command(command)
     ssh.close()
 
-
+    print("---accept touchUI---")
+    print("datetime : "+datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+    print("code : "+code)
 
