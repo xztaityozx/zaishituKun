26868db11fe60a8f241325778b5c4ffda242768f
xztaityozx <taityonohibi@gmail.com>
2017-06-13 15:50:34 +0900

    Add Web Page

--- RoR/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
--- RoR/vendor/bundle/ruby/2.4.0/gems/ffi-1.9.18/ext/ffi_c/libffi-x86_64-linux/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/zaisekiKun/zaishituKun/RoR/vendor/bundle/ruby/2.4.0/gems/ffi-1.9.18/ext/ffi_c/libffi/src/x86/ffitarget.h
